857c0daeb3e4595f32b4967f94229064849050d3
adriarodrisan <1900adria@gmail.com>
2023-11-28 13:26:24 +0100

    hamilton idiota

--- hello_world.py
+++ hello_world.py
@@ -3,4 +3,4 @@
 for i in range (0,33):
     print("alonso siempre padreando hasta en programación.")
     print("vertapen tiene que tener menos potencia en su red bull")
-    
+    print("hamilton siempre atascado en la grava")
