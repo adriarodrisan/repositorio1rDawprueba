dcab33db0092b8444673436adbaef4ecb06d8dd5
adriarodrisan <1900adria@gmail.com>
2023-11-28 13:37:21 +0100

    good

--- hello_world.py
+++ hello_world.py
@@ -4,4 +4,3 @@ for i in range (0,10):
     print("alonso siempre padreando hasta en programación.")
     print("vertapen tiene que tener menos potencia en su red bull")
     print("hamilton siempre atascado en la grava")
-    print("messirve")
