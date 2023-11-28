6058142ffcbe0d1ee5521ac92586e8b614f5bc9d
adriarodrisan <1900adria@gmail.com>
2023-11-28 13:30:23 +0100

    ok

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-for i in range (0,33):
+for i in range (0,10):
     print("alonso siempre padreando hasta en programación.")
     print("vertapen tiene que tener menos potencia en su red bull")
     print("hamilton siempre atascado en la grava")
