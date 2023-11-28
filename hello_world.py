81c933180c003b019f54de86b7ea349dd83f1f93
adriarodrisan <1900adria@gmail.com>
2023-11-28 13:24:29 +0100

    magic

--- hello_world.py
+++ hello_world.py
@@ -2,4 +2,5 @@
 
 for i in range (0,33):
     print("alonso siempre padreando hasta en programación.")
-    print(" vertapen tiene que tener menos potencia en su red bull")
+    print("vertapen tiene que tener menos potencia en su red bull")
+    
